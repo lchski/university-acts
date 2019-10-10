82e00f0b05394bf3a75cec6685cadb700b9b31af
Lucas Cherkewski <lucas@lucascherkewski.com>
2019-10-10 19:44:57 -0400

    ever more sophisticated splitting!

--- load.R
+++ load.R
@@ -4,12 +4,14 @@ purposes_raw <- read_csv("data/purposes.csv")
 
 purposes <- purposes_raw %>%
   filter(! is.na(text)) %>%
-  mutate(text = str_split(text, "\\(?[a-z0-9]{1}\\)")) %>%
+  mutate(text = str_split(text, regex("\\(?[a-z0-9]{1}\\)|(?<=:) ", perl = TRUE))) %>%
   unnest(cols = c(text)) %>%
   mutate(text = trimws(text)) %>%
+  filter(text != "") %>%
   group_by(university, year, section) %>%
   mutate(subsection = row_number()) %>%
   ungroup() %>%
-  select(university:section, subsection, text)
+  select(university:section, subsection, text) %>%
+  arrange(university, year, section, subsection)
 
 purposes %>% write_csv("data/out/purpose_clauses.csv")
