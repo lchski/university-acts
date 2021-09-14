1485cf8baf2de59fefb27867d5353ef6a83412e8
Lucas Cherkewski <lucas@lucascherkewski.com>
2021-09-14 10:40:27 -0400

    analysis to compare purposes

--- analyse.R
+++ analyse.R
@@ -10,9 +10,34 @@ data("stop_words")
 stop_words <- stop_words
 
 purposes_to_compare <- purposes %>%
-  mutate(id = paste(university, section, subsection, sep = "-"))
+  mutate(id = paste(university, year, section, subsection, sep = "-"))
 
-target_ids <- analyse_statement_similarity(
+compared_purposes <- analyse_statement_similarity(
   statements = purposes_to_compare,
   similarity_threshold = 0.75
-) %>% extract2("above_threshold_ids")
+)
+
+## Compare text of similar statements
+compared_purposes$above_threshold %>%
+  left_join(purposes_to_compare %>%
+              select(id, text), by = c("id.x" = "id")
+            ) %>%
+  rename(text.x = text) %>%
+  left_join(purposes_to_compare %>%
+              select(id, text), by = c("id.y" = "id")
+            ) %>%
+  rename(text.y = text) %>%
+  View()
+
+## Compare similar statements from the same institution
+compared_purposes$above_threshold %>%
+  left_join(purposes_to_compare %>%
+              select(id, text, university), by = c("id.x" = "id")
+  ) %>%
+  rename(text.x = text, university.x = university) %>%
+  left_join(purposes_to_compare %>%
+              select(id, text, university), by = c("id.y" = "id")
+  ) %>%
+  rename(text.y = text, university.y = university) %>%
+  filter(university.x == university.y) %>%
+  View()
